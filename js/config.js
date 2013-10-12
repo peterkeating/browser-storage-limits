debd5b8faa10003b74cc6414a319e3cdc6a3dbe6
Peter Keating <peter@moov2.com>
2013-10-12 23:09:34 +0100

    added start button for the kicking off the experiment.

--- js/config.js
+++ js/config.js
@@ -2,4 +2,8 @@ require.config({
 
     deps: [ 'main' ],
 
+    paths: {
+        jquery: 'vendor/jquery-1.10.2'
+    }
+
 });
--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 require([
-
+    'jquery'
 ],
 
-function () {
+function ($) {
 
-    // initialisation code goes here....
+    var $btnStart = $('.js-btn-start');
+
+    /**
+     * Starts the experiment.
+     */
+    var start = function () {
+
+    };
+
+    $(document).ready(function () {
+        // clicking the start button will begin the experiment.
+        $btnStart.on('click', start);
+    });
 
 });
