bb253f9242d5f5613cd2c7e202bd953daa6d8bad
Peter Keating <peter@moov2.com>
2013-10-16 10:50:42 +0100

    WebSQL doesn't support storing of blobs, when the indexedDB polyfill is being used the Blob is converted to base64 & compressed before being saved into the database.

--- js/storage/images.js
+++ js/storage/images.js
@@ -1,8 +1,9 @@
 define([
-    'storage/db'
+    'storage/db',
+    'storage/storageoptimization'
 ],
 
-function (Db) {
+function (Db, StorageOptimization) {
 
     /**
      * URL to image to be loaded.
@@ -12,6 +13,35 @@ function (Db) {
     function Images() { }
 
     var p = Images.prototype = {};
+
+    /**
+     * Below is an example of how to retrieve an image from local storage.
+     */
+    p.get = function (id, options) {
+        var transaction = Db.getTransaction(OBJECT_STORE),
+        store = transaction.objectStore(OBJECT_STORE);
+
+        var fixBinary = function (bin) {
+            var length = bin.length;
+            var buf = new ArrayBuffer(length);
+            var arr = new Uint8Array(buf);
+
+            for (var i = 0; i < length; i++) {
+                arr[i] = bin.charCodeAt(i);
+            }
+
+            return buf;
+        }
+
+        store.get(id).onsuccess = function (event) {
+            var data = fixBinary(atob(StorageOptimization.decompress(event.target.result)));
+            blob = new Blob([data], {type: 'image/png'});
+
+            if (options.success) {
+                options.success(blob);
+            }
+        };
+    };
 
     /**
      * Saves the provided image to the database.
@@ -35,11 +65,11 @@ function (Db) {
          * If the local database supports saving the blob straight to local storage
          * then we do that, otherwise a conversion to base 64 needs to happen first.
          */
-        if (Db.blobSupported) {
+        if (Db.blobSupported && !window.usingIndexedDBPolyfill) {
             put(image);
         } else {
             var reader = new FileReader();
-            reader.onloadend = function() { put(btoa(this.result)); };
+            reader.onloadend = function() { put(StorageOptimization.compress(btoa(this.result))); };
             reader.readAsBinaryString(image);
         }
     };
---
+++ js/storage/storageoptimization.js
@@ -0,0 +1,59 @@
+define([],
+
+/**
+ * Module is responsible for compressing string values that are to be stored in
+ * client storage. The source of this technique of compressing / decompressing
+ * base64 encoded strings can be found from the links below.
+ *
+ * http://labs.ft.com/2012/06/text-re-encoding-for-optimising-storage-capacity-in-the-browser/
+ * http://jsfiddle.net/morganestes/yjxtB/
+ */
+function () {
+
+    function StorageOptimization() { }
+
+    var p = StorageOptimization.prototype = {};
+
+    /**
+     * Compresses the provided string to reduce the amount of space it takes up
+     * in storage.
+     */
+    p.compress = function (s) {
+        var i, l, out = '';
+
+        if (s.length % 2 !== 0) {
+            s += ' ';
+        }
+
+        for (i = 0, l = s.length; i < l; i += 2) {
+            out += String.fromCharCode((s.charCodeAt(i) * 256) + s.charCodeAt(i + 1));
+        }
+
+        return String.fromCharCode(9731) + out;
+    };
+
+    /**
+     * Converts a compressed string into its original form.
+     */
+    p.decompress = function (s) {
+        var i, l, n, m, out = '';
+
+        /**
+         * checks to see if marker is used to determine whether the provided string
+         * has been created by the `compress` method.
+         */
+        if (s.charCodeAt(0) !== 9731) {
+            return s;
+        }
+
+        for (i = 1, l = s.length; i < l; i++) {
+            n = s.charCodeAt(i);
+            m = Math.floor(n / 256);
+            out += String.fromCharCode(m, n % 256);
+        }
+
+        return out;
+    };
+
+    return new StorageOptimization();
+});
